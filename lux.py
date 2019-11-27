5ab2cc0a46d33fb1ab8538f0ef772649e46ce83d
Uwe Jugel <uwe.jugel@lovoo.com>
2019-11-27 16:53:25 +0100

    inital commit

---
+++ lux.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+from luxafor import LuxaforFlag as LF
+from luxbus import Login
+import sys, tty, termios, fcntl, os
+from contextlib import contextmanager
+
+def usage():
+    print("""
+
+    Commands
+    ========
+    q|Q|Ctrl-C exit
+    h|H|u|U    show help/usage
+    x|X        turn LEDs off
+
+    Colors and Patterns
+    ===================
+    P    Police pattern
+    R    Rainbow pattern
+    w    white
+    r    red
+    g    green
+    b    blue
+    y    yellow
+    p    pink
+    c    cyan
+
+    """)
+
+# source of IO code: http://ballingt.com/nonblocking-stdin-in-python-3
+@contextmanager
+def raw(stream):
+    original_stty = termios.tcgetattr(stream)
+    try:
+        tty.setcbreak(stream)
+        yield
+    finally:
+        termios.tcsetattr(stream, termios.TCSANOW, original_stty)
+
+@contextmanager
+def nonblocking(stream):
+    fd = stream.fileno()
+    orig_fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    try:
+        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
+        yield
+    finally:
+        fcntl.fcntl(fd, fcntl.F_SETFL, orig_fl)
+
+def main():
+    lf = LF()
+    login = Login()
+    login.subscribers.append(lambda state: on_change_login_state(lf, state))
+    login.watch()
+    with raw(sys.stdin):
+        with nonblocking(sys.stdin):
+            main_loop(lf, login)
+
+def getch(): return str(sys.stdin.read(1))
+
+
+def on_change_login_state(lf, state):
+    print("changed state:", state)
+    if   state == Login.LOGGED_IN:  rgb = (0,   255,   0)
+    elif state == Login.LOGGED_OFF: rgb = (128,   0, 128)
+    else: return
+    lf.set_colors(rgb, None)
+
+def main_loop(lf, login):
+    print("LUXAFOR Control started, press Q to quit")
+    rgb, pat, rep = (255, 0, 0), None, 1
+    while True:
+        login.join(0.1)  # join the login watcher Thread instead of sleeping here
+        cmd = getch()
+        if   cmd in ("q", "Q"): break
+        elif cmd in ("h", "H"): usage()
+        elif cmd == "":   continue
+        elif cmd == "x":  lf.off(); continue
+        elif cmd == "P":  rgb, pat, rep = None, LF.PATTERN_POLICE,      1
+        elif cmd == "R":  rgb, pat, rep = None, LF.PATTERN_RAINBOWWAVE, 5
+        elif cmd == "w":  rgb, pat = (255, 255, 255), None
+        elif cmd == "r":  rgb, pat = (255,   0,   0), None
+        elif cmd == "g":  rgb, pat = (0,   255,   0), None
+        elif cmd == "b":  rgb, pat = (0,     0, 255), None
+        elif cmd == "y":  rgb, pat = (128, 128,   0), None
+        elif cmd == "p":  rgb, pat = (128,   0, 128), None
+        elif cmd == "c":  rgb, pat = (  0, 128, 128), None
+
+        # elif cmd == "rl": f.do_static_colour(LF.LED_ALL, 192,  32,  32); pat = None
+        # elif cmd == "gl": f.do_static_colour(LF.LED_ALL,  32, 192,  32); pat = None
+        # elif cmd == "bl": f.do_static_colour(LF.LED_ALL,  32,  32, 192); pat = None
+        lf.set_colors(rgb, pat, rep=rep)
+
+
+if __name__ == '__main__': main()
---
+++ luxbus.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from subprocess import Popen, PIPE
+from contextlib import contextmanager
+
+from threading import Thread
+import time
+
+DBUS_KEY_LOGIN = "org.freedesktop.login1"
+DBUS_MONITOR_COMMAND = "gdbus monitor -y -d".split(" ")
+
+@contextmanager
+def gdbus_monitor(key):
+    cmd = DBUS_MONITOR_COMMAND + [key]
+    with Popen(cmd, stdout=PIPE) as proc:
+        print("starting dbus monitor", cmd)
+        yield proc.stdout
+        print("stopping dbus monitor", cmd)
+
+class Login:
+    UNKNOWN    = 'UNKNOWN'
+    LOGGED_IN  = 'LOGGED_IN'
+    LOGGED_OFF = 'LOGGED_OFF'
+
+    state = LOGGED_IN
+    state_change_delay = 0.05  # 50ms
+
+    watcher:Thread = None
+    subscribers:list = None
+
+    def __init__(l):
+        l.subscribers = []
+
+    def read_dbus_line(l, line):
+        if   "'IdleHint': <true>"    in line: l.state = Login.LOGGED_OFF
+        elif "'IdleHint': <false>"   in line: l.state = Login.LOGGED_IN
+        elif "'LockedHint': <true>"  in line: l.state = Login.LOGGED_OFF
+        elif "'LockedHint': <false>" in line: l.state = Login.LOGGED_IN
+        else:
+            # no callback or delay required if state is not changed
+            return
+
+        print("line:", line, "state:", l.state)
+        for cb in l.subscribers: cb(l.state)
+        time.sleep(l.state_change_delay)
+
+    def read_dbus(l):
+        print("Login.watcher started")
+        with gdbus_monitor(DBUS_KEY_LOGIN) as stream:
+            for line in stream: l.read_dbus_line(str(line))
+        print("Login.watcher stopped")
+
+    def watch(l):
+        # send default state to subscribers
+        for cb in l.subscribers: cb(l.state)
+        # start watching state changes
+        l.watcher = t = Thread(target=l.read_dbus)
+        t.daemon = True
+        t.start()
+        return l
+
+    def join(l, timeout=None):
+        l.watcher.join(timeout=timeout)
+        return l
+
+def main(): Login().watch().join()
+
+if __name__ == '__main__': main()
